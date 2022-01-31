757000bbaf9882eedd6eb5371fd4355c72382ef8
barryforever <bharath.kumar.mail@gmail.com>
2022-01-31 11:53:43 +0530

    adding node components

---
+++ node-app/firstproject/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app =  express();
+
+var testVar = "test";
+console.log(testVar);
+
+/* ctrl+k+c : single line Comment
+ctrl+k+u : single line uncomment
+ctrl+alt+a : Multi line comment/uncomment */
+
